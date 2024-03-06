8bcc29d9e4433b25be426868493ef833aa7483af
MinaAlmasi <mina.almasi00@gmail.com>
2024-03-06 08:24:41 +0100

    add setup

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+python3 -m venv env
+source ./env/bin/activate
+
+echo "[INFO]: Installing necessary reqs in env" 
+pip install -r requirements.txt
+
+deactivate
+echo "[INFO]: Done!" 
