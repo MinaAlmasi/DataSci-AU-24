a5ebe53030fd341cb5b7b40f891a4455da777c1a
MinaAlmasi <mina.almasi00@gmail.com>
2024-04-17 09:35:37 +0200

    update setup for cnns

--- setup.sh
+++ setup.sh
@@ -6,5 +6,10 @@ source ./env/bin/activate
 echo "[INFO]: Installing necessary reqs in env" 
 pip install -r requirements.txt
 
+python -m ipykernel install --user --name=env
+
+# for torchviz
+sudo apt-get install graphviz
+
 deactivate
 echo "[INFO]: Done!" 
